c35876a450c0022d9482debfd2c293122bf29690
bhartidhotre <bhartidhotre65@gmail.com>
2026-03-28 15:08:27 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/auth";
+const API = "https://your-backend.onrender.com/api/auth";
 
 export const loginUser = async (data) => {
   const res = await axios.post(`${API}/login`, data);
